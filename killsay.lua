d6c7cafadceeeeba86da21b2996c68448f123617
k3ls <kelshvh@mail.ru>
2021-12-09 07:57:07 +0300

    Small tweaks for more intuitive navigation.

--- killsay.lua
+++ killsay.lua
@@ -3,20 +3,20 @@ local phrase = Menu.TextBox('Main', 'Custom KillSay', 128, '1')
 Cheat.RegisterCallback('events', function(event)
 	local me = EntityList.GetLocalPlayer()
     
-  if not EngineClient.IsInGame() or not me or me:GetProp('m_iHealth') <= 0 then
-    return
-  end
+	if not EngineClient.IsInGame() or not me or me:GetProp('m_iHealth') <= 0 then
+		return
+	end
     
-  if event:GetName() ~= 'player_death' then
-    return
-  end
+	if event:GetName() ~= 'player_death' then
+		return
+	end
 
-  local victim = EntityList.GetPlayerForUserID(event:GetInt('userid'))
-  local attacker = EntityList.GetPlayerForUserID(event:GetInt('attacker'))
+	local victim = EntityList.GetPlayerForUserID(event:GetInt('userid'))
+	local attacker = EntityList.GetPlayerForUserID(event:GetInt('attacker'))
 
-  if victim == attacker or attacker ~= me or victim:GetProp('m_iTeamNum') == me:GetProp('m_iTeamNum') then
-    return
-  end
+	if victim == attacker or attacker ~= me or victim:GetProp('m_iTeamNum') == me:GetProp('m_iTeamNum') then
+		return
+	end
 
-  EngineClient.ExecuteClientCmd('say "' .. phrase:Get() .. '"')
+	EngineClient.ExecuteClientCmd('say "' .. phrase:Get() .. '"')
 end)
