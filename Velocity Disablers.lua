f25054629916f65e9a6ec79cc11a609c7c731d44
k3ls <kelshvh@mail.ru>
2022-02-27 17:25:01 +0300

    Full Re-Code

--- Velocity Disablers.lua
+++ Velocity Disablers.lua
@@ -1,33 +1,53 @@
-local strafe = Menu.Switch("Velocity Disablers", "Disable Strafer", true)
-local lag = Menu.Switch("Velocity Disablers", "Disable Fake Lag", false)
-local limit = Menu.SliderInt("Velocity Disablers", "Velocity Limit", 3, 1, 255)
-Menu.Text("Velocity Disablers", "1-3 standing, 100-150 walking, 150+ running")
+local ui = {
+	main	= Menu.MultiCombo('Main', 'to Disable', {'Auto Strafe', 'Fake Lag'}, 0),
+	jump	= Menu.Switch('Main', 'Auto Strafe \'IN_JUMP\' Check', true, 'Allow strafe only when you pressed your jump button. SPACE for example.'),
+	limit	= Menu.SliderInt('Main', 'Velocity Limit', 3, 1, 10),
+}
 
-local autostrafe = Menu.FindVar("Miscellaneous", "Main", "Movement", "Auto Strafe")
-local fakelag = Menu.FindVar("Aimbot", "Anti Aim", "Fake Lag", "Enable Fake Lag")
+local nl = {
+	strafe	= Menu.FindVar('Miscellaneous', 'Main', 'Movement', 'Auto Strafe'),
+	lag	= Menu.FindVar('Aimbot', 'Anti Aim', 'Fake Lag', 'Enable Fake Lag'),
+}
 
-Cheat.RegisterCallback("prediction", function(cmd)
-	local localplayer = EntityList.GetClientEntity(EngineClient.GetLocalPlayer())
-	local velocity = Vector.new(localplayer:GetProp("DT_BasePlayer", "m_vecVelocity[0]"), localplayer:GetProp("DT_BasePlayer", "m_vecVelocity[1]"), localplayer:GetProp("DT_BasePlayer", "m_vecVelocity[2]"))
-	if localplayer:GetProp("DT_BasePlayer", "m_iHealth") <= 0 then return end
+local visibility = function()
+	local strafe = ui.main:GetBool(1)
+	local fl = ui.main:GetBool(2)
 	
-	if velocity:Length2D() < limit:Get() then
-		if strafe:Get() then
-			autostrafe:SetBool(false)
-		end
-		
-		if lag:Get() then
-			fakelag:SetBool(false)
-		end
-	else
-		local onground = (bit.band(localplayer:GetProp("DT_BasePlayer", "m_fFlags"), 1) == 1)
+	ui.jump:SetVisible(strafe)
+	ui.limit:SetVisible(strafe or fl)
+end
+visibility()
+ui.main:RegisterCallback(visibility)
+
+Cheat.RegisterCallback('pre_prediction', function(cmd)
+	local localplayer = EntityList.GetLocalPlayer()
 	
-		if strafe:Get() and onground == false then
-			autostrafe:SetBool(true)
-		end
-		
-		if lag:Get() then
-			fakelag:SetBool(true)
+	if not localplayer or localplayer == nil or not localplayer:IsAlive() then return end
+	
+	local v1	= bit.band(cmd.buttons, 8)		== 8
+	local v2	= bit.band(cmd.buttons, 16)		== 16
+	local v3	= bit.band(cmd.buttons, 512)	== 512
+	local v4	= bit.band(cmd.buttons, 1024)	== 1024
+	local jump	= bit.band(cmd.buttons, bit.lshift(1, 1))
+	local velocity	= Vector.new(localplayer:GetProp('m_vecVelocity[0]'), localplayer:GetProp('m_vecVelocity[1]'), localplayer:GetProp('m_vecVelocity[2]'))
+	local onground	= (bit.band(localplayer:GetProp('m_fFlags'), 1) == 1) == true
+	local in_move	= (v1 or v2 or v3 or v4)
+
+	if ui.main:GetBool(1) then
+		if velocity:Length2D() < ui.limit:Get() or onground then
+			nl.strafe:SetBool(false)
+		else
+			nl.strafe:SetBool(true)
 		end
 	end
+
+	if ui.main:GetBool(2) then
+		if velocity:Length2D() < ui.limit:Get() then
+			nl.lag:SetBool(false)
+		else
+			nl.lag:SetBool(true)
+		end
+	end
+	
+	if ui.jump:Get() and jump ~= 2 then nl.strafe:SetBool(false) end
 end)
